d8964936c4b14ba62201162c56f41957724dab35
Elev <Elev@CNCN-WS047>
2023-11-15 08:05:13 +0200

    Add project files.

---
+++ XD-2023/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD_2023
+{
+    public class Car
+    {
+        public string color;
+        public string name;
+        public int maxspeed;
+        public int year;
+
+        public Car(string nameName, string colorName, int yearName, int maxSpeedName)
+        { name = nameName;
+          color = colorName;
+          year = yearName;
+          maxspeed = maxSpeedName;
+        }
+        public Car(string nameName)
+        { name = nameName; }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + "is going at the speed of" + maxspeed);
+        }
+
+    }
+}
+
---
+++ XD-2023/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car Volvo = new Car("XC-90");
+            //Car Ford = new Car();
+            //Car Opel = new Car();
+            //Ford.name = "Mustang";
+            //Ford.year = 2021;
+            //Ford.color = "white";
+            //Ford.maxspeed = 100;
+
+            //Opel.name = "Astra";
+            //Opel.color = "Blue";
+            //Opel.year = 2020;
+            //Opel.maxspeed = 120;
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrottle();
+
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Opel.fullThrottle();
+
+        }
+    }
+}
